43f962d87bc87e9023f781f0a42247a035b2217e
Normand Martin <normandmartin@macbook-pro-de-normand-martin.local>
2016-05-15 15:22:13 -0400

    Working version

--- yearEquivalentnceForHistoryTimeLine/ViewController.swift
+++ yearEquivalentnceForHistoryTimeLine/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var arr: NSMutableArray = []
+var n: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let plist = Plist(name: "TotalTimeLine") {
+            arr = plist.getMutablePlistFile()!
+        }
+        
+        for arrs in arr {
+            var newHist = arrs[0] as! String
+            if  newHist.containsString("ka"){
+                newHist.removeAtIndex(newHist.endIndex.predecessor())
+                newHist.removeAtIndex(newHist.endIndex.predecessor())
+                newHist.removeAtIndex(newHist.endIndex.predecessor())
+                let ya = 1000 * Double(newHist)!
+                let BC = ya - 2000
+                let arrElement = arrs[0] as! String
+                let arrNewElement = "Year: \(arrElement)," + " \(Int(ya)) Ya," + " \(Int(BC)) BC"
+                arr[n] = [arrNewElement, arrs[1], arrs[2], arrs[3]]
+
+            }
+        n = n + 1
+        }
+        if let plist = Plist(name: "TotalTimeLine"){
+            do {
+                try plist.addValuesToPlistFile(arr)
+            } catch {
+                print(error)
+            }
+            //let newArr = plist.getValuesInPlistFile()!
+            //print(newArr)
+        }else{
+            print("unable to get plist")
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
